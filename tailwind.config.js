d8698379d7e9affd03ac8528a0173eff115ea332
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-10-31 13:18:39 -0400

    Update Tailwind CSS configuration to include content paths for EJS and JS files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./views/**/*.ejs",
+    "./public/**/*.js",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
